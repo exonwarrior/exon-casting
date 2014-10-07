37cf7863ebd9b05ec9770fd40f2c3bbcc4a7b308
Dave Haenze <dah33@aber.ac.uk>
2014-10-07 15:49:51 +0100

    Fixed typo. Learn to spell initialize

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function initializeCastApi() {
 	var applicationID = chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID;
 	var sessionRequest = new chrome.cast.SessionRequest(applicationID);
 	var apiConfig = new chrome.cast.ApiConfig(sessionRequest, sessionListener, receiverListener);
-	chrome.cast.initalize(apiConfig, onInitSuccess, onInitError);
+	chrome.cast.initialize(apiConfig, onInitSuccess, onInitError);
 };
 
 function sessionListener(e){
